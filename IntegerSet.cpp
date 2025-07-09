2633debe2cf384a47542f1e5135709696d8c1e9d
NINA <ninatsao@ningningning.lan>
2025-07-08 22:54:49 -0700

    6th commit

--- IntegerSet.cpp
+++ IntegerSet.cpp
@@ -2,22 +2,22 @@
 #include "IntegerSet.h"
 using namespace std;
 
-// Comment: The constructor initializes each elements to false
-//          in the set vector that hold integers in the range 0 - 100.
+// Comment 4: The constructor initializes each elements to false
+//            in the set vector that hold integers in the range 0 - 100.
 IntegerSet::IntegerSet()
     : set(SIZE, false)
 {
 }
 
-// Comment: Create a new set "result" as the third set and return its
-//          consequence without modifying either of them.
+// Comment 5: Create a new set "result" as the third set and return its
+//            consequence without modifying either of them.
 IntegerSet IntegerSet::unionOfSets(const IntegerSet &set2) const
 {
     IntegerSet result;
     for (int i = 0; i < SIZE; i++)
     {
-        // Comment: If the element is true in either set, set it
-        //          to true in the result, otherwise, set it to false.
+        // Comment 6: If the element is true in either set, set it
+        //            to true in the result, otherwise, set it to false.
         if (this->set[i] || set2.set[i])
             result.set[i] = true;
         else
@@ -31,8 +31,8 @@ IntegerSet IntegerSet::intersectionOfSets(const IntegerSet &set2) const
     IntegerSet result;
     for (int i = 0; i < SIZE; i++)
     {
-        // Comment: If the element is true in both sets, set it
-        //          to true in the result, otherwise, set it to false.
+        // Comment 7: If the element is true in both sets, set it
+        //            to true in the result, otherwise, set it to false.
         if (this->set[i] && set2.set[i])
             result.set[i] = true;
         else
@@ -41,7 +41,7 @@ IntegerSet IntegerSet::intersectionOfSets(const IntegerSet &set2) const
     return result;
 }
 
-// Comment: Set set[k] to true if the provided integer, k, is within the range.
+// Comment 8: Set set[k] to true if the provided integer, k, is within the range.
 void IntegerSet::insertElement(int k)
 {
     if (k >= 0 && k < SIZE)
@@ -50,7 +50,7 @@ void IntegerSet::insertElement(int k)
         cout << "Insert element is out of range." << endl;
 }
 
-// Comment: Set set[m] to false if the provided integer, m, is within the range.
+// Comment 9: Set set[m] to false if the provided integer, m, is within the range.
 void IntegerSet::deleteElement(int m)
 {
     if (m >= 0 && m < SIZE)
@@ -72,10 +72,11 @@ void IntegerSet::printSet() const
         }
     }
     if (isEmpty)
-        cout << "--";
-    cout << " }";
+        cout << "--- ";
+    cout << "}" << endl;
 }
 
+// Comment 10: Return to false if both sets' elements are not equal, otherwise, return to true.
 bool IntegerSet::isEqualTo(const IntegerSet &set2) const
 {
     for (int i = 0; i < SIZE; i++)
@@ -88,10 +89,31 @@ bool IntegerSet::isEqualTo(const IntegerSet &set2) const
 
 void IntegerSet::inputSet()
 {
-
+    int element;
+    cout << "Enter an element (-1 to end): ";
+    cin >> element;
+    while (element != -1)
+    {
+        if (element >= 0 && element < SIZE)
+            insertElement(element);
+        else
+            cout << "Invalid Element\n";
+        cout << "Enter an element (-1 to end): ";
+        cin >> element;
+    }
+    cout << "Entry complete\n";    
 }
 
+// Comment 11: The constructor initializes all elements in the range 0 - 100 to false.
+//             And if any element in the array is within the range, set it to true.
 IntegerSet::IntegerSet(const int array[], int arraySize)
+    : set(SIZE, false)
 {
-
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (array[i] >= 0 && array[i] < SIZE)
+            set[array[i]] = true;
+        else
+            cout << "Invalid insert attempted!" << endl;
+    }
 }
--- IntegerSet.h
+++ IntegerSet.h
@@ -9,21 +9,21 @@ class IntegerSet
 public:
     IntegerSet();
 
-    // Return an new union (new IntegerSet) of two sets, so the return data type should be IntegerSet.
-    // Passing another set (IntegerSet's object) to do a union, and ensure it will not be modified (const).
-    // The const at the end ensures the current object (the one calling the function) will not be modified.
+    // Comment 1: Return an new union (new IntegerSet) of two sets, so the return data type should be IntegerSet.
+    //            Passing another set (IntegerSet's object) to do a union, and ensure it will not be modified (const).
+    //            The const at the end ensures the current object (the one calling the function) will not be modified.
     IntegerSet unionOfSets(const IntegerSet &set2) const;
     IntegerSet intersectionOfSets(const IntegerSet &set2) const;
     void insertElement(int k);
     void deleteElement(int m);
     void printSet() const;
 
-    // Check if all elements of two sets are equal to each other, the const at the end
-    // ensures the current object (the one calling the function) will not be modified.
+    // Comment 2: Check if all elements of two sets are equal to each other. The const at the end
+    //            ensures the current object (the one calling the function) will not be modified.
     bool isEqualTo(const IntegerSet &set2) const;
     void inputSet();
 
-    // Additional constructor that receives an array of integers and its size.
+    // Comment 3: Additional constructor that receives an array of integers and its size.
     IntegerSet(const int array[], int arraySize);
 private:
     std::vector<bool> set;
