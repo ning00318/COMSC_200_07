c10154e069821b94892198adb9a0d1553a4913eb
NINA <ninatsao@ningningning.lan>
2025-07-08 20:30:17 -0700

    3rd commit

--- IntegerSet.cpp
+++ IntegerSet.cpp
@@ -2,19 +2,43 @@
 #include "IntegerSet.h"
 using namespace std;
 
+// Comment: The constructor initializes each elements to false
+//          in the set vector that hold integers in the range 0 - 100.
 IntegerSet::IntegerSet()
+    : set(101, false)
 {
-
 }
 
-IntegerSet IntegerSet::unionOfSets(const IntegerSet &set) const
+// Comment: Create a new set "result" as the third set and return its
+//          consequence without modifying either of them.
+IntegerSet IntegerSet::unionOfSets(const IntegerSet &set2) const
 {
-
+    IntegerSet result;
+    for (int i = 0; i < set.size(); i++)
+    {
+        // Comment: If the element is true in either set, set it
+        //          to true in the result, otherwise, set it to false.
+        if (this->set[i] || set2.set[i])
+            result.set[i] = true;
+        else
+            result.set[i] = false;
+    }
+    return result;
 }
 
-IntegerSet IntegerSet::intersectionOfSets(const IntegerSet &set) const
+IntegerSet IntegerSet::intersectionOfSets(const IntegerSet &set2) const
 {
-
+    IntegerSet result;
+    for (int i = 0; i < set.size(); i++)
+    {
+        // Comment: If the element is true in both sets, set it
+        //          to true in the result, otherwise, set it to false.
+        if (this->set[i] && set2.set[i])
+            result.set[i] = true;
+        else
+            result.set[i] = false;
+    }
+    return result;
 }
 
 void IntegerSet::insertElement(int k)
@@ -32,7 +56,7 @@ void IntegerSet::printSet() const
 
 }
 
-bool IntegerSet::isEqualTo(const IntegerSet &set) const
+bool IntegerSet::isEqualTo(const IntegerSet &set2) const
 {
 
 }
--- IntegerSet.h
+++ IntegerSet.h
@@ -12,15 +12,15 @@ public:
     // Return an new union (new IntegerSet) of two sets, so the return data type should be IntegerSet.
     // Passing another set (IntegerSet's object) to do a union, and ensure it will not be modified (const).
     // The const at the end ensures the current object (the one calling the function) will not be modified.
-    IntegerSet unionOfSets(const IntegerSet &set) const;
-    IntegerSet intersectionOfSets(const IntegerSet &set) const;
+    IntegerSet unionOfSets(const IntegerSet &set2) const;
+    IntegerSet intersectionOfSets(const IntegerSet &set2) const;
     void insertElement(int k);
     void deleteElement(int m);
     void printSet() const;
 
     // Check if all elements of two sets are equal to each other, the const at the end
     // ensures the current object (the one calling the function) will not be modified.
-    bool isEqualTo(const IntegerSet &set) const;
+    bool isEqualTo(const IntegerSet &set2) const;
     void inputSet();
 
     // Additional constructor that receives an array of integers and its size.
