3939b2c9c4ffd74555ccc4c7c6f6e9b7055aa48a
NINA <ninatsao@ningningning.lan>
2025-07-08 19:35:51 -0700

    2nd commit

---
+++ IntegerSet.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "IntegerSet.h"
+using namespace std;
+
+IntegerSet::IntegerSet()
+{
+
+}
+
+IntegerSet IntegerSet::unionOfSets(const IntegerSet &set) const
+{
+
+}
+
+IntegerSet IntegerSet::intersectionOfSets(const IntegerSet &set) const
+{
+
+}
+
+void IntegerSet::insertElement(int k)
+{
+
+}
+
+void IntegerSet::deleteElement(int m)
+{
+
+}
+
+void IntegerSet::printSet() const
+{
+
+}
+
+bool IntegerSet::isEqualTo(const IntegerSet &set) const
+{
+
+}
+
+void IntegerSet::inputSet()
+{
+
+}
+
+IntegerSet::IntegerSet(const int array[], int arraySize)
+{
+
+}
---
+++ IntegerSet.h
@@ -0,0 +1,32 @@
+#ifndef INTEGERSET_H
+#define INTEGERSET_H
+
+#include <iostream>
+#include <vector>
+
+class IntegerSet
+{
+public:
+    IntegerSet();
+
+    // Return an new union (new IntegerSet) of two sets, so the return data type should be IntegerSet.
+    // Passing another set (IntegerSet's object) to do a union, and ensure it will not be modified (const).
+    // The const at the end ensures the current object (the one calling the function) will not be modified.
+    IntegerSet unionOfSets(const IntegerSet &set) const;
+    IntegerSet intersectionOfSets(const IntegerSet &set) const;
+    void insertElement(int k);
+    void deleteElement(int m);
+    void printSet() const;
+
+    // Check if all elements of two sets are equal to each other, the const at the end
+    // ensures the current object (the one calling the function) will not be modified.
+    bool isEqualTo(const IntegerSet &set) const;
+    void inputSet();
+
+    // Additional constructor that receives an array of integers and its size.
+    IntegerSet(const int array[], int arraySize);
+private:
+    std::vector<bool> set;
+};
+
+#endif
